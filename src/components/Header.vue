dbc2943aada3cda00000180732173b04b4d5de3c
cristianworth <cristianweissmantel@gmail.com>
2021-08-15 17:06:19 -0300

    Add maska package. Add header e footer

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-header>
+        <el-menu :router="true" :default-active="$route.path" mode="horizontal">
+            <el-menu-item
+            :key="index"
+            :index="rule.path"
+            v-for="(rule, index) in $router.options.routes">
+            <i :class="rule.icon"></i
+            ><span class="menu-item-name">{{ rule.name }}</span>
+            </el-menu-item>
+        </el-menu>
+    </el-header>
+</template>
