3f8fe24f4462e3f854ff66af2a370cf37c8e484b
cristianworth <cristianweissmantel@gmail.com>
2021-08-16 10:41:22 -0300

    Add firebase e CRUD usuario

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+import 'firebase/auth'
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCD7ZDoslb0BvgZYNI-G6hxez8VSpzchI8",
+    authDomain: "frida-f6343.firebaseapp.com",
+    databaseURL: "https://frida-f6343-default-rtdb.firebaseio.com",
+    projectId: "frida-f6343",
+    storageBucket: "frida-f6343.appspot.com",
+    messagingSenderId: "127916459378",
+    appId: "1:127916459378:web:546f32e5936d4e7b1e09eb",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+//collection references
+const usuariosCollection = db.collection('usuarios')
+const vitimaCollection = db.collection('vitima')
+const agressorCollection = db.collection('agressor')
+
+export {
+    db,
+    auth,
+    usuariosCollection,
+    vitimaCollection,
+    agressorCollection
+}
