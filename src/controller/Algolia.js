ff4ebc522b8173e98ad89f9d70f4a8c867dcf43d
MarceloMilbradt <milbradthz@gmail.com>
2021-12-13 20:25:14 -0300

    formatado o texto kekw

--- src/controller/Algolia.js
+++ src/controller/Algolia.js
@@ -1,28 +1,29 @@
 
 const al = require("algoliasearch");
 
-const client = al("0AMQ3RWXQS", "dedc497f0397bbdc6778e5f538d2cce8");
+const client = al(
+  "0AMQ3RWXQS", "dedc497f0397bbdc6778e5f538d2cce8"
+  );
 const index = client.initIndex("frida_denuncias");
 
 client.clearCache()
 
 const doRefresh = (client) =>{
-  console.log('refresh')
-  console.log(client.clearCache())
+  client.clearCache()
 }
 
 const doSearch = async (term) => { 
-  await doRefresh();
+  doRefresh();
   await index.search(term) 
 };
 
 const addToIndex = (data,objectID) => {
       return index.saveObject({...data, objectID});
-    };
+};
 
 const updateIndex = (data,objectID) => {
       return index.saveObject({...data, objectID});
-    };
+};
 
 const deleteFromIndex = (objectID) => index.deleteObject(objectID);
 
